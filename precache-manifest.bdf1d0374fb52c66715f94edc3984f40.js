f80bbeeebda0ad60182ae0cf55ca6d6d94567694
Naeun <knae11@naver.com>
2020-09-14 10:58:08 +0900

    Updates

---
+++ precache-manifest.bdf1d0374fb52c66715f94edc3984f40.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc68fb418ca8601f1dd82a1716d4d1cd",
+    "url": "/porfolio-3.-Quiz/index.html"
+  },
+  {
+    "revision": "ae36b88a820babb8b6e4",
+    "url": "/porfolio-3.-Quiz/static/js/2.a0d91059.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/porfolio-3.-Quiz/static/js/2.a0d91059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab9e9344678407c95997",
+    "url": "/porfolio-3.-Quiz/static/js/main.63823d59.chunk.js"
+  },
+  {
+    "revision": "e6180f18bad6842171ef",
+    "url": "/porfolio-3.-Quiz/static/js/runtime-main.074bbcf3.js"
+  }
+]);
